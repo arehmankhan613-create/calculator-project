2097d3598cd5d4e906f2d1083f88f0f9e571c7cf
arehmankhan613-create <arehmankhan613@gmail.com>
2026-07-30 16:03:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,14 @@ function clearDisplay() {
     display.value = "";
 }
 
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
 function calculate() {
     try {
         display.value = eval(display.value);
-    } catch {
+    } catch (error) {
         display.value = "Error";
     }
 }
